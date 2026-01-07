fa7fc9468e91d5ed280bef695fba69536d625cab
DuyCao1708 <tucaoduygl@gmail.com>
2026-01-07 17:07:53 +0700

    Basically decorated login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login';
+import { Categories } from './components/categories';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Login },
+  { path: 'categories', component: Categories },
+];
